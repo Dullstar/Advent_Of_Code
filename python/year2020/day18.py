9f5dd7450c5d37845a01937f65b21a042a411a33
Dullstar <62787412+Dullstar@users.noreply.github.com>
2021-11-28 04:03:50 -0600

    Python days 11-15, 18-20
    With this, all days that were implemented in Python from the original 2020 repository have been added. The days that remain need to be translated from C++ or D.

--- python/year2020/day18.py
+++ python/year2020/day18.py
@@ -1,15 +1,182 @@
 import time
 import os
+import enum
+
+
+class Operator(enum.Enum):
+    ADD = enum.auto()
+    MUL = enum.auto()
+
+
+# Note that this problem involves changing the order of operations.
+# A lot of these are static methods packaged with this class for organization purposes - having them keep the object's
+# state intact allows us to more safely reuse bits and pieces in Part 2.
+
+# An older version of this script had cleaner parsing, but it assumed all numbers in the input file were between
+# 0 and 9, which was probably fine, but the problem description never explicitly states this.
+# It's probably possible to clean this up again, but it would require rewriting the parsing.
+class Expression:
+    def __init__(self, string: str):
+        self.terms = []
+        check = True
+        resume_check = -1
+        string = self.remove_spaces(string)
+        last_was_num = False
+        current_num_str = ""
+        for i, char in enumerate(string):
+            if check:
+                if char.isdigit():
+                    current_num_str += char
+                    last_was_num = True
+                    continue
+                elif last_was_num:
+                    self.terms.append(current_num_str)
+                    # print("Appended the number: ", current_num_str)
+                    current_num_str = ""
+                    last_was_num = False
+                if char == "(":
+                    check = False
+                    expr, resume_check = self.find_subexpression(string, i)
+                    self.terms.append(expr)
+                elif char == "+" or char == "*":
+                    self.terms.append(char)
+                else:
+                    raise IOError(f"Invalid character in input: {char}")
+            elif i >= resume_check:
+                check = True
+        if last_was_num:
+            self.terms.append(current_num_str)
+        self._result = None
+        self._result_2 = None
+
+    @property
+    def result(self):
+        """Gets the result of the expression."""
+        if self._result is not None:
+            return self._result
+        else:
+            self._result = self.evaluate(self.terms, 1)
+            return self._result
+
+    @property
+    def result_2(self):
+        """Gets the result of the expression for Part 2, where addition is to be done first"""
+        if self._result_2 is not None:
+            return self._result_2
+        else:
+            self._result_2 = self.evaluate_2(self.terms)
+            return self._result_2
+
+    @staticmethod
+    def find_subexpression(string: str, offset: int):
+        """Creates a subexpression from parentheses given the location of the starting parenthesis.
+        Returns the expression and the index of the final parenthesis"""
+        to_ignore = 0  # to_ignore is used to handle nested parentheses
+        offset += 1  # We don't need to check the original character that triggered this call
+        for i, char in enumerate(string[offset:], offset):
+            if char == "(":
+                to_ignore += 1
+            if char == ")":
+                if to_ignore > 0:
+                    to_ignore -= 1
+                    continue
+                else:
+                    return Expression(string[offset:i]), i  # those nested parentheses we ignored get handled here
+        print(string[offset:])
+        raise IOError(
+            "Triggered find_subexpression, but didn't find an end. Either the code is wrong or a parenthesis "
+            "in the original expression is mismatched."
+        )
+
+    @staticmethod
+    def remove_spaces(string: str) -> str:
+        """Removes spaces from the given expression and returns the result."""
+        output = ""
+        for char in string:
+            if char != " ":
+                output += char
+        return output
+
+    @staticmethod
+    def evaluate(terms: list, rule_set: int):
+        """Evaluates the expression given by an already-parsed list of terms. Can be explicitly called, but it's better
+        to call this through the result property, which will make the call if it needs to."""
+        running_total = 0
+        current_operand = Operator.ADD  # defaulting to add will make it convenient to get started
+        for term in terms:
+            if term == "+":
+                current_operand = Operator.ADD
+                continue
+            if term == "*":
+                current_operand = Operator.MUL
+                continue
+
+            if type(term) == Expression:
+                if rule_set == 1:
+                    term = term.result
+                elif rule_set == 2:
+                    term = term.result_2
+                else:
+                    raise ValueError(f"Unrecognized rule set ID: {rule_set}")
+
+            term = int(term)
+
+            if current_operand is Operator.ADD:
+                running_total += term
+            elif current_operand is Operator.MUL:
+                running_total *= term
+            else:
+                raise ValueError("No operator currently loaded. This should never happen.")
+        return running_total
+
+    @staticmethod
+    def evaluate_2(terms: list):
+        """Evaluates the expression by the second rule_set. Like the evaluate function, it can be explicitly called,
+        but it's still better to call this through the result_2 property, which will make the call if it needs to."""
+
+        add_terms = []
+        mul_terms = []
+        for term in terms:
+            if term == "*":
+                mul_terms.append(Expression.evaluate(add_terms, 2))
+                add_terms = []
+            else:
+                add_terms.append(term)
+        mul_terms.append(Expression.evaluate(add_terms, 2))
+        product = 1
+        for term in mul_terms:
+            # Notice that mul_term's contents are only created by appending the result of evaulate(), which returns
+            # an int: thus, we don't need to worry about the possibility of finding an expression in here.
+            product *= term
+
+        return product
+
+
+def parse_input(filename):
+    with open(filename, "r") as file:
+        expressions = [Expression(expression) for expression in [line.strip() for line in file]]
+    return expressions
 
 
 def main(input_filename: str):
     if not os.path.exists(input_filename):
         raise FileNotFoundError(f"Couldn't find input file: {input_filename}")
-    # Generated by generate_files.py on Wed Nov 17 2021
 
-    # Return value of -1 is used to signal that this isn't implemented.
-    # Once it is, remove/replace that return (implicitly returning None is fine).
-    return -1
+    start_time = time.time()
+    expressions = parse_input(input_filename)
+    part1_start = time.time()
+    results1 = [expression.result for expression in expressions]
+    print(f"All the expressions left to right sum to {sum(results1)}")
+    part2_start = time.time()
+    results2 = [expression.result_2 for expression in expressions]
+    print(f"All the expressions addition before multiplication sum to {sum(results2)}")
+    end_time = time.time()
+
+    print("Elapsed Time:")
+    print(f"    Parsing: {(part1_start - start_time) * 1000:.2f} ms")
+    print(f"    Part 1: {(part2_start - part1_start) * 1000:.2f} ms")
+    print(f"    Part 2: {(end_time - part2_start) * 1000:.2f} ms")
+    print(f"    Total: {(end_time - start_time) * 1000:.2f} ms")
 
 
 if __name__ == "__main__":
--- python/year2020/day20.py
+++ python/year2020/day20.py
@@ -1,15 +1,333 @@
 import time
 import os
+import re
+import enum
+import sys
+import queue as q
+
+
+# -- START TILES SUBSECTION -- #
+# (This section was once its own file in the original repository)
+# At a certain point, Day 20 transitioned from, "Let's do this well!" to "Just get it done..."
+# Here is where the ickiness is hidden away.
+
+class Direction(enum.Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    NORTH_FLIP = 4
+    EAST_FLIP = 5
+    SOUTH_FLIP = 6
+    WEST_FLIP = 7
+
+
+class Tile:
+    def __init__(self, string_form: str):
+        string_form = string_form.split("\n")
+        tile_id = re.search(r"[0-9]+", string_form[0])
+        self.tile_id = tile_id.group(0)
+        self.width = len(string_form[1])
+        self.height = len(string_form) - 1
+        self.contents = []
+        for t_str in string_form[1:]:
+            for c in t_str:
+                self.contents.append(0 if c == "." else 1)
+        self.flipped = False
+        self.rotation = Direction.NORTH
+        self.borders = {}
+        self._init_borders()
+        self.location = None
+
+    def __getitem__(self, key: tuple):
+        orig_x = self.width - 1 - key[0] if self.flipped else key[0]
+        orig_y = key[1]
+        if self.rotation == Direction.NORTH:
+            x = orig_x
+            y = orig_y
+        elif self.rotation == Direction.EAST:
+            x = orig_y
+            y = self.height - 1 - orig_x
+        elif self.rotation == Direction.SOUTH:
+            x = self.width - 1 - orig_x
+            y = self.height - 1 - orig_y
+        elif self.rotation == Direction.WEST:
+            x = self.width - 1 - orig_y
+            y = orig_x
+        else:
+            print("A tile is an an invalid state", file=sys.stderr)
+            raise
+        return self.contents[y * self.width + x]
+
+    def __str__(self):
+        output = ""
+        output += f"Tile {self.tile_id}:\n"
+        total = 0
+        for c in self.contents:
+            if total == self.width:
+                output+="\n"
+                total = 0
+            output += "." if c == 0 else "#"
+            total += 1
+        return output
+
+    def _init_borders(self):
+        # I wanted the rotation logic to not be dependent on the width = height assumption to make it a useful guide
+        # for later re-use, but the border stuff requires it now to make sense.
+        if self.width != self.height:
+            print(f"A tile's height ({self.height}) did not equal its width ({self.width}).", file=sys.stderr)
+            raise
+        # Stash the starting state here
+        store_rotation = self.rotation
+        store_flip = self.flipped
+
+        directions = [Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST,
+                      Direction.NORTH_FLIP, Direction.SOUTH_FLIP, Direction.EAST_FLIP, Direction.WEST_FLIP]
+        for direction in directions:
+            # I'm sure there's a better way.
+            if direction == Direction.NORTH_FLIP:
+                self.rotation = Direction.NORTH
+                self.flipped = True
+            elif direction == Direction.EAST_FLIP:
+                self.rotation = Direction.EAST
+                self.flipped = True
+            elif direction == Direction.SOUTH_FLIP:
+                self.rotation = Direction.SOUTH
+                self.flipped = True
+            elif direction == Direction.WEST_FLIP:
+                self.rotation = Direction.WEST
+                self.flipped = True
+            else:
+                self.rotation = direction
+                self.flipped = False
+
+            bin_string = ""
+            for x in range(self.width):
+                bin_string += str(self[x, 0])
+
+            self.borders[direction] = int(bin_string, 2)
+
+        # Put the starting state back
+        self.rotation = store_rotation
+        self.flipped = store_flip
+
+    def place_if_border_matches(self, other):
+        assert self.location is not None, "This shouldn't get called if self.location is None"
+        match = None
+        for self_key, self_value in self.borders.items():
+            if match is not None:
+                break
+            for other_key, other_value in other.borders.items():
+                if self_value == other_value:
+                    match = self_key, other_key
+                    break
+
+        if match:
+            if match[1] == Direction.NORTH_FLIP or match[1] == Direction.SOUTH_FLIP \
+                    or match[1] == Direction.WEST_FLIP or match[1] == Direction.EAST_FLIP:
+                other.flipped = True
+
+            rel_location = self._find_relative_coordinates(match)
+            # print("RL: ", rel_location, "SL: ", self.location)
+            other.location = self.location[0] + rel_location[0], self.location[1] + rel_location[1]
+            self._rotate_other(other, match)
+            return True
+        return False
+
+    @staticmethod
+    def _find_relative_coordinates(match: tuple[Direction, Direction]):
+        # EAST and WEST flip are weird in terms of accounting.
+        if match[0] == Direction.NORTH or match[0] == Direction.NORTH_FLIP:
+            return 0, -1
+        if match[0] == Direction.SOUTH or match[0] == Direction.SOUTH_FLIP:
+            return 0, 1
+        # I expected west would get -1 and east would get 1, but experimentally, we want the other way around for
+        # reasons I don't fully understand. Still, it would seem we should get a working result by swapping them,
+        # so that'll do.
+        if match[0] == Direction.WEST or match[0] == Direction.WEST_FLIP:
+            return 1, 0
+        if match[0] == Direction.EAST or match[0] == Direction.EAST_FLIP:
+            return -1, 0
+        raise ValueError("Somehow, _find_relative_coordinates failed to find a result.")
+
+    # Can't think of a good name for this function. Its purpose is to make things easier by rotating the entire
+    # contents such that rotation can be set to north, so we won't have to account for it when figuring out where to
+    # place neighboring tiles.
+    def perma_rotate(self):
+        new_contents = []
+        for y in range(self.height):
+            for x in range(self.width):
+                new_contents.append(self[x, y])
+        self.contents = new_contents
+        self.rotation = Direction.NORTH
+        self.flipped = False
+        self._init_borders()
+
+    def _rotate_other(self, other, match: tuple[Direction, Direction]):
+        # This isn't a good way to do this, really. Ideally, we'd want to precompute this and then select the correct
+        # option. But quite frankly I was getting confused thinking about the relative rotations, and this works well
+        # enough for this problem. If this were production code we'd want to do it right, but since it's not I'll just
+        # make it work.
+        # print(f"Source Tile {self.tile_id}: Rotate other Tile {other.tile_id}: ", match)
+        for f in [False, True]:
+            other.flipped = f
+            if f:
+                other.perma_rotate()
+            for i in range(4):
+                other.rotation = Direction.EAST
+                other.perma_rotate()
+
+                # Experimentally, the FLIP directions don't seem to come up for match[0]
+                # I think that's bcause of some relative stuff going on that causes other to always be flip and
+                # never this one. Or maybe it's just a coincidence that could bite on specific inputs.
+
+                # The way we've defined the flips and border directions may be easy to explain, but then it leads
+                # to weirdness like this.
+                if match[0] == Direction.EAST and self.borders[Direction.EAST] == other.borders[Direction.WEST_FLIP]:
+                    return
+                if match[0] == Direction.WEST and self.borders[Direction.WEST] == other.borders[Direction.EAST_FLIP]:
+                    return
+                if match[0] == Direction.SOUTH and self.borders[Direction.SOUTH] == other.borders[Direction.NORTH_FLIP]:
+                    return
+                if match[0] == Direction.NORTH and self.borders[Direction.NORTH] == other.borders[Direction.SOUTH_FLIP]:
+                    return
+
+        print("This shouldn't be possible to reach! The offending match value was: ", match, file=sys.stderr)
+        assert False
+
+    def _search_for_sea_monsters_subroutine(self):
+        # Format: Each internal list is a y coordinate relative to origin (top left) of sea monster
+        # Each number in the list is the corresponding x coordinate.
+        SEA_MONSTER = [[18], [0, 5, 6, 11, 12, 17, 18, 19], [1, 4, 7, 10, 13, 16]]
+        SEA_MONSTER_SIZE_X = 20
+        SEA_MONSTER_SIZE_Y = 3
+
+        changed = False
+
+        def check_lines(_x: int, _y: int):
+            _match_coords = []
+            # Check the lines
+            for sea_monster_y in range(SEA_MONSTER_SIZE_Y):
+                for sea_monster_x in SEA_MONSTER[sea_monster_y]:
+                    if self[sea_monster_x + _x, sea_monster_y + _y] != 1:
+                        return None
+                    else:
+                        _match_coords.append((sea_monster_x + _x, sea_monster_y + _y))
+            return _match_coords
+
+        for y in range(self.height - (SEA_MONSTER_SIZE_Y + 1)):
+            for x in range(self.width - (SEA_MONSTER_SIZE_X + 1)):
+                if (match_coords := check_lines(x, y)) is not None:
+                    changed = True
+                    for coord in match_coords:
+                        # Setting to 0 allows us to get the answer simply by doing a sum,
+                        # but it also means we have no way to visualize this.
+                        self.contents[coord[1] * self.width + coord[0]] = 0
+
+        return changed
+
+    def search_for_sea_monsters(self):
+        skip = False
+        for i in range(4):
+            if self._search_for_sea_monsters_subroutine():
+                skip = True
+                break
+            self.rotation = Direction.EAST
+            self.perma_rotate()
+        self.flipped = True
+        self.perma_rotate()
+        for i in range(4):
+            if skip or self._search_for_sea_monsters_subroutine():
+                break
+            self.rotation = Direction.EAST
+            self.perma_rotate()
+
+        print("Part 2: Water roughness", sum(self.contents))
+
+# -- END TILES SUBSECTION -- #
+
+
+class Image:
+    def __init__(self, tilepool: list[Tile]):
+        self.tilepool = tilepool
+        tilepool[0].location = (0, 0)
+        self.layout = {(0, 0): tilepool[0]}
+        self.tile_size = self.tilepool[0].width
+        self.full_layout = ""
+        self.part_1_answer = 0
+        queue = q.SimpleQueue()
+        queue.put(tilepool[0])
+
+        while not queue.empty():
+            tile: Tile = queue.get()
+            for other_tile in tilepool:
+                if other_tile.location is None and tile.place_if_border_matches(other_tile):
+                    self.layout[other_tile.location] = other_tile  # it won't be None anymore here
+                    queue.put(other_tile)
+
+        self.finalize()
+        self.full_layout = Tile("Tile 0000:\n" + self.full_layout.strip())
+
+    def __str__(self):
+        # for key in sorted(self.layout.keys(), key=self._tile_location_compare):
+        return self.layout.__str__()
+
+    def finalize(self):
+        coords = {}
+        x_set = set()
+        for key in self.layout.keys():
+            x_set.add(key[0])
+            if key[1] in coords:
+                coords[key[1]].append(key)
+            else:
+                coords[key[1]] = [key]
+
+        for y in sorted(coords.keys()):
+            coords[y].sort()
+            for j in range(1, self.tile_size - 1):
+                for coord in coords[y]:
+                    for i in range(1, self.tile_size - 1):
+                        self.full_layout += "#" if self.layout[coord][i, j] == 1 else "."
+                self.full_layout += "\n"
+
+        # print(self.full_layout)
+
+        x_min = min(x_set)
+        x_max = max(x_set)
+        y_min = min(coords.keys())
+        y_max = max(coords.keys())
+        self.part_1_answer = int(self.layout[x_min, y_min].tile_id) * int(self.layout[x_max, y_min].tile_id) \
+            * int(self.layout[x_min, y_max].tile_id) * int(self.layout[x_max, y_max].tile_id)
+
+        print("Part 1: Product of corners: ", self.part_1_answer)
+
+
+def parse_input(filename: str):
+    with open(filename, "r") as file:
+        tile_strings = file.read().strip().split("\n\n")
+        tiles = []
+        for tile_string in tile_strings:
+            tiles.append(Tile(tile_string))
+
+    return tiles
 
 
 def main(input_filename: str):
     if not os.path.exists(input_filename):
         raise FileNotFoundError(f"Couldn't find input file: {input_filename}")
-    # Generated by generate_files.py on Wed Nov 17 2021
 
-    # Return value of -1 is used to signal that this isn't implemented.
-    # Once it is, remove/replace that return (implicitly returning None is fine).
-    return -1
+    start_time = time.time()
+    tiles = parse_input(input_filename)
+    part1_start = time.time()
+    image = Image(tiles)
+    part2_start = time.time()
+    image.full_layout.search_for_sea_monsters()
+    end_time = time.time()
+    print("Elapsed Time:")
+    print(f"    Parsing: {(part1_start - start_time) * 1000:.2f} ms")
+    print(f"    Part 1: {(part2_start - part1_start) * 1000:.2f} ms")
+    print(f"    Part 2: {(end_time - part2_start) * 1000:.2f} ms")
+    print(f"    Total: {(end_time - start_time) * 1000:.2f} ms")
 
 
 if __name__ == "__main__":
