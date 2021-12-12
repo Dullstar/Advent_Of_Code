2a07788a8e3595026ee849829482a2846d86218e
Dullstar <62787412+Dullstar@users.noreply.github.com>
2021-12-11 22:53:38 -0600

    Add C++ launcher
    No days are implemented yet and parsing is complete, but all the files are at least there, and they compile.

---
+++ cpp/premake5.lua
@@ -0,0 +1,29 @@
+workspace "Advent_Of_Code"
+    configurations {"debug", "release"}
+
+project "Advent_Of_Code"
+	kind "ConsoleApp"
+	language "C++"
+	architecture ("x86_64")
+	cppdialect ("C++17")
+
+	--dependencies
+	files {"deps/fmt/include/**.h", "deps/fmt/src/format.cc", "deps/fmt/src/os.cc"}  -- fmt.cc is intentionally excluded
+    files {"deps/dstr/include/**.h", "deps/dstr/src/**.cpp"}
+	includedirs {"deps/fmt/include"}
+    includedirs {"deps/dstr/include"}
+	
+	--project files
+	files {"source/**.h", "source/**.cpp"}
+
+    filter "configurations:debug"
+        targetdir "bin-debug"
+		defines {"DEBUG"}
+		symbols "On"
+
+    filter "configurations:release"
+        targetdir "bin"
+		optimize "Speed"
+
+	filter "toolset:gcc or toolset:clang"  --todo: clang complains about -fmax-errors, but will compile anyway
+		buildoptions {"-Wall", "-Wextra", "-pedantic", "-fmax-errors=5"}
