aa77ee054ba3905ea2f47f4a0cb74af1dd4e7131
Dullstar <62787412+Dullstar@users.noreply.github.com>
2023-12-03 03:43:08 -0600

    Update utility.d

--- D/source/utility.d
+++ D/source/utility.d
@@ -36,36 +36,28 @@ template Grid2D(T, S = int)
             layout.length = size.x * size.y;
             layout[] = default_fill;
         }
-        deprecated this(S size_x, S size_y, T default_fill = T.init)
-        {
-            this(Point!S(size_x, size_y), default_fill);
-        }
+
         this(Point!S size_, T[] layout_)
         {
             layout = layout_;
             size = size_;
+            assert (layout_.length == size_.x * size_.y);
         }
-        deprecated this(S size_x, S size_y, T[] layout_)
-        {
-            this(Point!S(size_x, size_y), layout_);
-        }
-        T opIndex(S x, S y)
+
+        T opIndex(S x, S y) const
         // in(x >= 0 && y >= 0 && x < size_x && x < size_y, format("%d, %d out of bounds! (must be between 0, 0 and %d, %d)"))
         {
             return layout[index_at_pt(Point!S(x, y))];
         }
 
-        T opIndex(Point!S pt)
+        T opIndex(Point!S pt) const
         {
             return layout[index_at_pt(pt)];
         }
+
         void opIndexAssign(T val, Point!S pt)
         {
             layout[index_at_pt(pt)] = val;
-        }
-        deprecated void opIndexAssign(T val, S x, S y)
-        {
-            layout[index_at_pt(Point!S(x, y))] = val;
         }
 
         Point!S pt_at_index(size_t i) const
@@ -79,10 +71,6 @@ template Grid2D(T, S = int)
             return (pt.y * size.x + pt.x).to!size_t;
         }
 
-        deprecated size_t index_at_pt(S x, S y) const
-        {
-            return index_at_pt(Point!S(x, y));
-        }
         bool in_bounds(Point!S pt) const
         {
             return pt.x >= 0 && pt.y >= 0 && pt.x < size.x && pt.y < size.y;
@@ -91,18 +79,3 @@ template Grid2D(T, S = int)
         T[] layout;
     }
 }
-
-/*
-
-// I have yet to figure out why this won't compile. It seems to dislike the calls to writeln.
-mixin template duration_report()
-{
-    writeln("Elapsed Time:");
-    // We request usecs and then convert to msecs for extra precision.
-    // If we request msecs, we just get an integer value.
-    writefln("    Parsing: %s ms", float((pt1_start - start_time).total!"usecs") / 1000);
-    writefln("    Part 1: %s ms", float((pt2_start - pt1_start).total!"usecs") / 1000);
-    writefln("    Part 2: %s ms", float((end_time - pt2_start).total!"usecs") / 1000);
-    writefln("    Total: %s ms", float((end_time - start_time).total!"usecs") / 1000);
-}
-*/
